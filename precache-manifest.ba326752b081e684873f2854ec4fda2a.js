936e9021641d739c29c8a289c4c9e0d5cd8c055b
id github: RafaBie <homefac@gmail.com>
2023-01-30 13:48:06 +0100

    Updates

---
+++ precache-manifest.ba326752b081e684873f2854ec4fda2a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "42877a2f0ef2cf265254",
+    "url": "/portfolio/static/css/main.71078658.chunk.css"
+  },
+  {
+    "revision": "42877a2f0ef2cf265254",
+    "url": "/portfolio/static/js/main.42877a2f.chunk.js"
+  },
+  {
+    "revision": "561126de61029355b316",
+    "url": "/portfolio/static/js/1.561126de.chunk.js"
+  },
+  {
+    "revision": "d2ea0006394243bab127",
+    "url": "/portfolio/static/js/2.d2ea0006.chunk.js"
+  },
+  {
+    "revision": "17d0ddd4111c1383f698",
+    "url": "/portfolio/static/js/runtime~main.17d0ddd4.js"
+  },
+  {
+    "revision": "7f6f2ed1b09ae6ca98ea81fd131f917c",
+    "url": "/portfolio/static/media/Rafal_Bierzanek_02_2023.7f6f2ed1.pdf"
+  },
+  {
+    "revision": "a3c3b1f7f8580cff6ff385565ad4f3dc",
+    "url": "/portfolio/static/media/ME.a3c3b1f7.png"
+  },
+  {
+    "revision": "33b8527a5f7fea0c9be01262821cd0a7",
+    "url": "/portfolio/static/media/weather-app.33b8527a.png"
+  },
+  {
+    "revision": "cf0f6d84ccf48fa7a9a83da25c817ea9",
+    "url": "/portfolio/static/media/todos.cf0f6d84.png"
+  },
+  {
+    "revision": "f13b7c79632169b71ecd947cf62bbb1a",
+    "url": "/portfolio/static/media/redesign.f13b7c79.png"
+  },
+  {
+    "revision": "a3c3b1f7f8580cff6ff385565ad4f3dc",
+    "url": "/portfolio/static/media/ME2.a3c3b1f7.png"
+  },
+  {
+    "revision": "d881395683502f5fa60f19adabd55bb4",
+    "url": "/portfolio/static/media/fanrec.d8813956.png"
+  },
+  {
+    "revision": "764b539d6bab8252af343d0f76b46f2e",
+    "url": "/portfolio/index.html"
+  }
+];
